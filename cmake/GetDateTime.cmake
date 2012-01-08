fde31fd5743e17289e2e42416f0584cdc182c2fa
David C. Bishop <david@davidbishop.org>
2012-01-09 05:42:56 +1100

    Wrap comments at 80 characters.

--- cmake/GetDateTime.cmake
+++ cmake/GetDateTime.cmake
@@ -1,14 +1,22 @@
 # GetDateTime.cmake - Sets a string with the current datetime.
 # Written in 2011 by David Bishop <david@davidbishop.org>
 #
-# To the extent possible under law, the author(s) have dedicated all copyright and related and neighboring rights to this software to the public domain worldwide. This software is distributed without any warranty.
-# You should have received a copy of the CC0 Public Domain Dedication along with this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain
+# worldwide. This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along with
+# this software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>
 
-# NOTE: This is the timestamp from the time cmake was run, not the compile time...
+# [NOTE]: This is the timestamp from the time cmake was run, not the compile
+# time...
 
 macro(DATETIME RESULT)
     if(WIN32)
-        # NOTE: Windows has a differnt date format to everything else and will probably change the date/time format depending on the internationalization settings. Possible work around would be to use a vbs script via cscript.exe.
+        # [NOTE]: Windows has a differnt date format to everything else and will
+        # probably change the date/time format depending on the
+        # internationalization settings. Possible work around would be to use a
+        # vbs script via cscript.exe.
         execute_process(COMMAND "echo" "%DATE%" OUTPUT_VARIABLE RESULT_DATE)
         execute_process(COMMAND "echo" "%TIME%" OUTPUT_VARIABLE RESULT_TIME)
         string(STRIP ${RESULT_DATE} RESULT_DATE)
